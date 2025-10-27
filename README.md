23f446c10151a7ec9feb08bb7131928866d04613
Playify <shulkerhd@gmail.com>
2025-10-27 14:15:48 +0100

    Fixed default values for popup.html

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ This Chrome extension allows downloading from a few video sites.
 ## Helper Program
 
 For better support, go into the NativeMessaging/_run folder, and run the exe file.
-That way, ffmpeg can be started from js
+That way, ffmpeg can be started from js. (needs ffmpeg inside the PATH environment variable)
--- chrome_plugin/js/popup.js
+++ chrome_plugin/js/popup.js
@@ -4,12 +4,12 @@ for (let button of document.querySelectorAll("button[data-action]")) {
     button.addEventListener("click", () => chrome.runtime.sendMessage({ action }));
 }
 const useFFmpeg = document.querySelector("#useFFmpeg");
-chrome.storage.local.get("useFFmpeg").then(b => useFFmpeg.checked = b.useFFmpeg);
+chrome.storage.local.get("useFFmpeg").then(b => useFFmpeg.checked = b.useFFmpeg ?? false);
 useFFmpeg.addEventListener("click", () => chrome.storage.local.set({ useFFmpeg: useFFmpeg.checked }));
 const useSeriesFolder = document.querySelector("#useSeriesFolder");
-chrome.storage.local.get("useSeriesFolder").then(b => useSeriesFolder.checked = b.useSeriesFolder);
+chrome.storage.local.get("useSeriesFolder").then(b => useSeriesFolder.checked = b.useSeriesFolder ?? false);
 useSeriesFolder.addEventListener("click", () => chrome.storage.local.set({ useSeriesFolder: useSeriesFolder.checked }));
 const maxFfmpeg = document.querySelector("#maxFfmpeg");
-chrome.storage.local.get("maxFfmpeg").then(b => maxFfmpeg.value = b.maxFfmpeg);
+chrome.storage.local.get("maxFfmpeg").then(b => maxFfmpeg.value = b.maxFfmpeg ?? 0);
 maxFfmpeg.addEventListener("input", () => chrome.storage.local.set({ maxFfmpeg: maxFfmpeg.value || undefined }));
 //# sourceMappingURL=popup.js.map
